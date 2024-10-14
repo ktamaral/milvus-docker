dd0acb0b6bea2aa2faaec88bfa6cd790cf3f021b
Katie Amaral <katie_amaral@harvard.edu>
2024-10-14 19:40:28 -0400

    Milvus setup

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # milvus-docker
+
+https://milvus.io/docs/install_standalone-docker-compose.md
+
+```
+python3 -m venv milvus-docker
+source milvus-docker/bin/activate
+pip install -r requirements.txt
+```
---
+++ create_collection.py
@@ -0,0 +1,6 @@
+if client.has_collection(collection_name="demo_collection"):
+    client.drop_collection(collection_name="demo_collection")
+client.create_collection(
+    collection_name="demo_collection",
+    dimension=768,  # The vectors we will use in this demo has 768 dimensions
+)
